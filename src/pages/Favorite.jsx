0a55caa32e20ea42eb65418af65001c45a304cf6
khanhhoang1997 <khanhlatom@gmail.com>
2024-12-29 17:16:15 +0700

    fix UI

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -2,19 +2,35 @@ import React from 'react'
 import { getLocalStorageWithExpiration } from '~/utils/localStorageUtils';
 import GridMovie from '~/components/GridMovie';
 import { useAppContext } from '~/contexts/AppContext';
+import { Link } from 'react-router-dom';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import { Button } from '@mui/material';
+
 
 function Favorite() {
   const [page, setPage] = React.useState(1);
 
   const { movies } = useAppContext();
   console.log('getMovie', movies);
-  
 
-  
+  if (!movies) {
+    return (
+      <>
+        <h2 style={{ color: 'white' }}>Danh sách đang trống, hãy lưu bất kì bộ nào bạn muốn và trở lại đây nhé!</h2>
+        <Button variant="contained" sx={{ borderRadius:'20px', padding: '10px 40px'  }}>
+          <Link to={'/filter'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}><LiveTvIcon /> <span style={{ marginLeft: '9px', fontWeight:'bold' }}>Xem Tại đây</span></Link>
+        </Button>
+      </>
+    )
+
+  }
+
+
+
   return (
     <div>
-      <GridMovie 
-        page={page} 
+      <GridMovie
+        page={page}
         data={movies}
         total={movies.length}
         onHandlePagination={(_page) => setPage(_page)}
