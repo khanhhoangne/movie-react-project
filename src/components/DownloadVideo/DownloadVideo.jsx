145c5d312728ab86fc52f3e5d03230ee3dd5af9f
khanhhoang1997 <khanhlatom@gmail.com>
2024-12-30 11:07:28 +0700

    fix UI

--- src/components/DownloadVideo/DownloadVideo.jsx
+++ src/components/DownloadVideo/DownloadVideo.jsx
@@ -7,16 +7,18 @@ import httpRequest from '~/utils/httpRequest';
 
 const fetchAndDownloadVideo = async (m3u8Url) => {
   try {
-    m3u8Url = m3u8Url.replace('http://', 'https://');
+    // Fetch the .m3u8 file using your Axios instance
     const response = await httpRequest.get(m3u8Url);
     const m3u8Content = response.data;
 
+    // Parse the .m3u8 file to extract segment URLs
     const baseUrl = m3u8Url.substring(0, m3u8Url.lastIndexOf('/') + 1);
     const segmentUrls = m3u8Content
       .split('\n')
       .filter((line) => line && !line.startsWith('#'))
       .map((line) => baseUrl + line);
 
+    // Fetch and combine all video segments using Axios
     const videoSegments = await Promise.all(
       segmentUrls.map((url) =>
         httpRequest
@@ -25,6 +27,7 @@ const fetchAndDownloadVideo = async (m3u8Url) => {
       )
     );
 
+    // Create a Blob from the combined segments
     const blob = new Blob(videoSegments, { type: 'video/mp4' });
     const a = document.createElement('a');
     a.href = URL.createObjectURL(blob);
--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -85,7 +85,7 @@ function Watch() {
                 }
 
 
-                <DownloadVideo source={linkEmbed} />
+                {/* <DownloadVideo source={linkEmbed} /> */}
 
             </div>
         )
